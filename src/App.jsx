91d003fcdd19cab740ced01d4f19c2e723462c1f
Jonathan Pynes <jon.pynes@shineforth.co>
2022-07-07 19:01:04 -0500

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import Logo from "./Logo"
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <AppCss>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sun animation to correspond to time.
         </p>
+   
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          We are going to build a morning habits 
         </a>
+              <Logo />
       </header>
-    </div>
+    </AppCss>
   );
 }
 
 export default App;
+
+const AppCss = styled.div`
+background-color: grey;
+`
